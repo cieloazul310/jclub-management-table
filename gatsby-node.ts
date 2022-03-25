5b16b61384b9433cddc34cd3d183a1abec2ddf53
cieloazul310 <cieloazul310@gmail.com>
2022-03-25 17:24:05 +0900

    fix: fix incorrect meta seo tags

---
+++ gatsby-node.ts
@@ -0,0 +1,9 @@
+export { createPages, createResolvers, createSchemaCustomization, sourceNodes } from './gatsby-node-dist/index';
+/*
+const { createPages, createResolvers, createSchemaCustomization, sourceNodes } = require('./gatsby-node-dist');
+
+exports.createPages = createPages;
+exports.createResolvers = createResolvers;
+exports.createSchemaCustomization = createSchemaCustomization;
+exports.sourceNodes = sourceNodes;
+*/
